6767bc261bf90970ff7b3c2218fa34f48e2017e6
Fayas7 <fayasfayaz707@gmail.com>
2026-06-24 15:17:29 +0530

    Complete experiment 03 bit flip noise analysis

--- src/noise_models.py
+++ src/noise_models.py
@@ -1,5 +1,22 @@
 from qiskit_aer.noise import NoiseModel
 from qiskit_aer.noise import depolarizing_error
+from qiskit_aer.noise import pauli_error
+def create_bit_flip_noise(probability):
+
+    noise_model = NoiseModel()
+
+    error = pauli_error([
+        ('X', probability),
+        ('I', 1 - probability)
+    ])
+
+    noise_model.add_all_qubit_quantum_error(
+        error,
+        ['measure']
+    )
+
+    return noise_model
+
 
 def create_depolarizing_noise(probability):
     """
